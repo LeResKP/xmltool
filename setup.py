640ea0fb89a59d697936fc9ba4c5267eb2e00629
Aurélien Matouillot <a.matouillot@gmail.com>
2012-09-13 01:16:17 +0200

    Package structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='xml-forms/',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Aur\xc3\xa9lien Matouillot',
+      author_email='a.matouillot@gmail.com',
+      url='https://github.com/LeResKP/xml-forms',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
