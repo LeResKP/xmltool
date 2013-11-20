14a1064dd1fa5512372a25c153ddfcce66d9a7d8
Aurélien Matouillot <a.matouillot@gmail.com>
2013-11-20 23:08:24 +0100

    Just reorder the functions

--- webmedia/src/xmltool.js
+++ webmedia/src/xmltool.js
@@ -6,7 +6,6 @@ if (typeof xmltool === 'undefined') {
 
 (function($){
 
-
     function Xmltool(element, options){
         this.options = options;
         // TODO: should be in options:
@@ -106,6 +105,123 @@ if (typeof xmltool === 'undefined') {
         this.delete_node('tree_' + $parent.attr('id'));
     };
 
+    Xmltool.prototype.delete_node = function(node_id){
+        var tree = $('#tree');
+        var node = tree.find('#' + xmltool.utils.escape_id(node_id));
+        if (node.length > 1){
+            alert('Too many values to delete');
+        }
+        var css_class = node.attr('class').split(' ')[0];
+        var nexts = node.nextAll('.'+xmltool.utils.escape_id(css_class));
+        var longprefix = xmltool.utils.get_prefix(node.attr('id'));
+        var prefix = xmltool.utils.get_prefix(longprefix);
+        xmltool.utils.decrement_id(prefix, nexts);
+        tree.jstree('delete_node', node);
+    };
+
+    Xmltool.prototype.set_btn_event = function(p){
+        var that = this;
+        p.find('textarea').autosize().focus(
+            function(){
+                var id = xmltool.utils.escape_id($(this).parent().attr('id'));
+                var tree = $('#tree');
+                tree.jstree('hover_node', $('#tree_' + id));
+                $(this).on('keyup.xmltool', function(){
+                    // TODO: this method should be improved to make
+                    // sure the user has made an update
+                    this.$form.trigger('field_change.xmltool');
+                });
+            }).blur(function(){
+                var id = xmltool.utils.escape_id($(this).parent().attr('id'));
+                var a = $('#tree_' + id).find('a');
+                var elt = a.find('._tree_text');
+                if (elt.length === 0){
+                    elt = $('<span class="_tree_text"/>').appendTo(a);
+                }
+                if($(this).val()){
+                    elt.text(' (' + xmltool.utils.truncate($(this).val(), 30) + ')');
+                }
+                else{
+                    elt.text('');
+                }
+                $(this).unbind('keyup.xmltool');
+            });
+
+        var set_fielset = function(fieldsets){
+            fieldsets.togglefieldset().bind('hide.togglefieldset', function(e){
+                var o = $('#tree_' + xmltool.utils.escape_id($(this).attr('id')));
+                $('#tree').jstree("close_node", o);
+                return false;
+            }).bind('show.togglefieldset', function(e){
+                var o = $('#tree_' + xmltool.utils.escape_id($(this).attr('id')));
+                $('#tree').jstree("open_node", o);
+                return false;
+            });
+        };
+
+        set_fielset(p.find('fieldset'));
+        if(p.is('fieldset')){
+            set_fielset(p);
+        }
+
+        p.find('.btn-delete').on('click', function(){
+            that.removeElement($(this));
+        });
+
+        p.find('a.btn-add').on('click', function(){
+            that.addElement($(this));
+        });
+        p.find('select.btn-add').on('change', function(){
+            that.addElement($(this));
+        });
+
+        p.find('.btn-comment').on('click', function(){
+            var self = $(this);
+
+            var comment_textarea = self.next('._comment');
+
+            if (!comment_textarea.length){
+                comment_textarea = $('<textarea>').attr('name', self.data('comment-name')).addClass('_comment');
+                self.after(comment_textarea);
+            }
+            // Create the dialog
+            var modal = self.data('modal');
+            if(modal === undefined){
+                $.ajax({
+                    type: 'GET',
+                    url: that.options.comment_modal_url,
+                    data: {'comment': comment_textarea.val()},
+                    dataType: 'json',
+                    async: false,
+                    success: function(data, textStatus, jqXHR){
+                        modal = $(data.content);
+                        self.data('modal', modal);
+                        modal.find('.submit').click(function(){
+                            var value = modal.find('textarea').val();
+                            comment_textarea.val(value);
+                            that.options.close_dialog(modal);
+
+                            self.attr('title', value);
+                            if (value){
+                                self.addClass('has-comment');
+                            }
+                            else{
+                                self.removeClass('has-comment');
+                            }
+                            return false;
+                        });
+                    },
+                    error: function(jqXHR, textStatus, errorThrown){
+                        // TODO: Replace all this kind of errors!
+                        console.log('Error ajax loading');
+                    }
+                });
+            }
+            that.options.open_dialog(modal);
+            return false;
+         });
+        };
+
 
     var default_options = {
         open_dialog: function(dialog){
@@ -117,129 +233,9 @@ if (typeof xmltool === 'undefined') {
     };
 
     $.fn.xmltool = function(options){
-        var self = $(this);
-        var settings = $.extend({}, default_options, options);
-
-        var xt = new Xmltool(this, settings);
-
-
-        Xmltool.prototype.delete_node = function(node_id){
-            var tree = $('#tree');
-            var node = tree.find('#' + xmltool.utils.escape_id(node_id));
-            if (node.length > 1){
-                alert('Too many values to delete');
-            }
-            var css_class = node.attr('class').split(' ')[0];
-            var nexts = node.nextAll('.'+xmltool.utils.escape_id(css_class));
-            var longprefix = xmltool.utils.get_prefix(node.attr('id'));
-            var prefix = xmltool.utils.get_prefix(longprefix);
-            xmltool.utils.decrement_id(prefix, nexts);
-            tree.jstree('delete_node', node);
-        };
-
-        Xmltool.prototype.set_btn_event = function(p){
-                    p.find('textarea').autosize().focus(
-                        function(){
-                            var id = xmltool.utils.escape_id($(this).parent().attr('id'));
-                            var tree = $('#tree');
-                            tree.jstree('hover_node', $('#tree_' + id));
-                            $(this).on('keyup.xmltool', function(){
-                                // TODO: this method should be improved to make
-                                // sure the user has made an update
-                                self.trigger('field_change.xmltool');
-                            });
-                        }).blur(function(){
-                            var id = xmltool.utils.escape_id($(this).parent().attr('id'));
-                            var a = $('#tree_' + id).find('a');
-                            var elt = a.find('._tree_text');
-                            if (elt.length === 0){
-                                elt = $('<span class="_tree_text"/>').appendTo(a);
-                            }
-                            if($(this).val()){
-                                elt.text(' (' + xmltool.utils.truncate($(this).val(), 30) + ')');
-                            }
-                            else{
-                                elt.text('');
-                            }
-                            $(this).unbind('keyup.xmltool');
-                        });
-
-            var set_fielset = function(fieldsets){
-                fieldsets.togglefieldset().bind('hide.togglefieldset', function(e){
-                    var o = $('#tree_' + xmltool.utils.escape_id($(this).attr('id')));
-                    $('#tree').jstree("close_node", o);
-                    return false;
-                }).bind('show.togglefieldset', function(e){
-                    var o = $('#tree_' + xmltool.utils.escape_id($(this).attr('id')));
-                    $('#tree').jstree("open_node", o);
-                    return false;
-                });
-            };
-
-            set_fielset(p.find('fieldset'));
-            if(p.is('fieldset')){
-                set_fielset(p);
-            }
-
-            p.find('.btn-delete').on('click', function(){
-                xt.removeElement($(this));
-            });
-
-            p.find('a.btn-add').on('click', function(){
-                xt.addElement($(this));
-            });
-            p.find('select.btn-add').on('change', function(){
-                xt.addElement($(this));
-            });
-
-            p.find('.btn-comment').on('click', function(){
-                var self = $(this);
-
-                var comment_textarea = self.next('._comment');
-
-                if (!comment_textarea.length){
-                    comment_textarea = $('<textarea>').attr('name', self.data('comment-name')).addClass('_comment');
-                    self.after(comment_textarea);
-                }
-                // Create the dialog
-                var modal = self.data('modal');
-                if(modal === undefined){
-                    $.ajax({
-                        type: 'GET',
-                        url: settings.comment_modal_url,
-                        data: {'comment': comment_textarea.val()},
-                        dataType: 'json',
-                        async: false,
-                        success: function(data, textStatus, jqXHR){
-                            modal = $(data.content);
-                            self.data('modal', modal);
-                            modal.find('.submit').click(function(){
-                                var value = modal.find('textarea').val();
-                                comment_textarea.val(value);
-                                settings.close_dialog(modal);
-
-                                self.attr('title', value);
-                                if (value){
-                                    self.addClass('has-comment');
-                                }
-                                else{
-                                    self.removeClass('has-comment');
-                                }
-                                return false;
-                            });
-                        },
-                        error: function(jqXHR, textStatus, errorThrown){
-                            // TODO: Replace all this kind of errors!
-                            console.log('Error ajax loading');
-                        }
-                    });
-                }
-                settings.open_dialog(modal);
-                return false;
-             });
-            };
-
         return this.each(function(){
+            var settings = $.extend({}, default_options, options);
+            var xt = new Xmltool(this, settings);
             xt.set_btn_event($(this));
         });
     };
