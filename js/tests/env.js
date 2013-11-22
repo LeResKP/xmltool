3806b0bb37eaa729dab980d7b7b26cb7aeb878d6
Aurélien Matouillot <a.matouillot@gmail.com>
2013-11-22 19:45:20 +0100

    Remove old javascript folder

--- js/tests/env.js
+++
@@ -1,8 +0,0 @@
-var jsdom = require('jsdom').jsdom;
-window = jsdom().createWindow();
-document = window.document;
-navigator = window.navigator
-// We don't care of the confirm dialog in the test
-confirm = function(text){return true;};
-// Fake alert
-alert = function(text){console.log(text);};
