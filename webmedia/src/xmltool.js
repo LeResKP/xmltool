e309dfac055e3f4706775b506eaa5d75f489756d
Aurélien Matouillot <a.matouillot@gmail.com>
2013-11-29 22:50:02 +0100

    Use the correct object

--- webmedia/src/xmltool.js
+++ webmedia/src/xmltool.js
@@ -162,7 +162,7 @@ if (typeof xmltool === 'undefined') {
             },
             error: function(jqXHR, textStatus, errorThrown){
                 var msg = jqXHR.status + ' ' + jqXHR.statusText;
-                this.message('error', msg);
+                that.message('error', msg);
             }
         });
     };
@@ -290,7 +290,7 @@ if (typeof xmltool === 'undefined') {
                     },
                     error: function(jqXHR, textStatus, errorThrown){
                         var msg = jqXHR.status + ' ' + jqXHR.statusText;
-                        this.message('error', msg);
+                        that.message('error', msg);
                     }
                 });
             }
